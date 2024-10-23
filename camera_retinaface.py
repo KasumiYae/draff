e2470ecf567f79813ac82ed3089063842cca8cae
KasumiYae <kiriha0205@gmail.com>
2024-10-23 16:59:03 +0700

    new file camera test

--- camera_retinaface.py
+++ camera_retinaface.py
@@ -9,19 +9,44 @@ class RetinaFaceLayer(Layer):
     def __init__(self, **kwargs):
         super(RetinaFaceLayer, self).__init__(**kwargs)
         self.model = RetinaFace.build_model()
+    
     def call(self, inputs):
         outputs = self.model(inputs)
         bbox_outputs = outputs[0]  # Giả định đầu ra đầu tiên là bbox
         return bbox_outputs
 
 # Tải mô hình đã fine-tuned
-model = load_model('/home/kasumi/Home/code/group_project/Main/retinaface/fine_tuned_retinaface.h5', custom_objects={'RetinaFaceLayer': RetinaFaceLayer})
+model = load_model('/home/kasumi/Home/code/group_project/Main/retinaface/model.h5', custom_objects={'RetinaFaceLayer': RetinaFaceLayer})
 
-# Hàm để nhận diện khuôn mặt từ một khung hình
+# Kích thước input của mô hình (cần phải giống với kích thước mà mô hình được huấn luyện)
+input_size = (128, 128)
+
+# Hàm để nhận diện khuôn mặt từ một khung hình sử dụng mô hình đã fine-tuned
 def detect_face(frame):
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    detections = RetinaFace.detect_faces(frame_rgb)
-    return detections
+    # Resize frame to match model input size
+    resized_frame = cv2.resize(frame, input_size)
+    
+    # Normalize the frame (if necessary, e.g., scaling pixel values between 0-1)
+    normalized_frame = resized_frame / 255.0
+    
+    # Add batch dimension (required by the model)
+    input_tensor = np.expand_dims(normalized_frame, axis=0)
+    
+    # Predict bounding boxes using the fine-tuned model
+    predicted_boxes = model.predict(input_tensor)[0]  # [0] to remove batch dimension
+    
+    # Scale predicted boxes back to the original frame size
+    height, width, _ = frame.shape
+    scale_x = width / input_size[0]
+    scale_y = height / input_size[1]
+    
+    # Scale the bounding box coordinates
+    x_min = int(predicted_boxes[0] * scale_x)
+    y_min = int(predicted_boxes[1] * scale_y)
+    x_max = int(predicted_boxes[2] * scale_x)
+    y_max = int(predicted_boxes[3] * scale_y)
+    
+    return (x_min, y_min, x_max, y_max)
 
 # Khởi tạo camera
 cap = cv2.VideoCapture(0)
@@ -31,13 +56,11 @@ while True:
     if not ret:
         break
     
-    # Nhận diện khuôn mặt
-    detections = detect_face(frame)
+    # Nhận diện khuôn mặt từ khung hình
+    x_min, y_min, x_max, y_max = detect_face(frame)
     
-    # Vẽ bounding box quanh các khuôn mặt
-    for key in detections:
-        facial_area = detections[key]['facial_area']
-        cv2.rectangle(frame, (facial_area[0], facial_area[1]), (facial_area[2], facial_area[3]), (0, 255, 0), 2)
+    # Vẽ bounding box quanh khuôn mặt
+    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
     
     # Hiển thị khung hình
     cv2.imshow('Face Detection', frame)
